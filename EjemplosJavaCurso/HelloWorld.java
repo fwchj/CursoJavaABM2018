06e1e6f65cf6483e7272777e248aee51bd732283
fwchj <florian.chavez@cide.edu>
2018-02-21 11:15:14 -0600

    Upload HelloWorld.java
    Very simple 'hello world' file in Java. It simply prints the text to the console. As exercise, you can compile and run it from the command line rather than using an IDE.

---
+++ EjemplosJavaCurso/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+This is our very first program in Java. 
+The goal is to print the text 'Hello World' 
+in the command line
+*/
+
+public class HelloWorld{
+	// Main method begins the execution of the Java program
+	public static void main(String[] args){
+		System.out.println("Hello world"); // Prints the text to the console
+		
+	} // end of the method main
+} // end class HelloWorld
+
+
+
+
+
